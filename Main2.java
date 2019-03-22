d744ef0cd0ab0caaf2186e460729368813158dad
millerk10 <millerk10@moravian.edu>
2019-03-22 12:54:10 -0400

    Updated Main

---
+++ Main2.java
@@ -0,0 +1,9 @@
+public class Main2 {
+
+    public static void main(String[] args) {
+        RandomAddresses RandomList = new RandomAddresses();
+        RandomList.GenerateAddresses();
+        ReadAddresses.TimePriority();
+        TruckRoute.Main();
+    }
+}
